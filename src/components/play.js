0157f101b753cf20da8f4c07343be3b6e95036f0
A R Bradley <arb@As-MacBook-Air.frontierlocal.net>
2019-04-05 19:57:38 -0500

    done with play function

--- src/components/play.js
+++ src/components/play.js
@@ -11,11 +11,112 @@ export default class Play extends Component {
     };
   }
   componentDidMount() {
-    let { maze, row, column } = this.state;
-    maze[row - 1][column - 1].show = <div id="dot" />;
+    let { maze, row, column, i, colors } = this.state;
+    maze[row - 1][column - 1].show = <div className="dot" />;
     this.setState({ maze });
   }
-  moveMarker(direction) {}
+  moveMarker(direction) {
+    let { maze, row, column, i, colors } = this.state;
+    if (
+      (direction === "up" && row > 1) ||
+      (direction === "left" && column > 1) ||
+      (direction === "down" && row < this.props.element.rows) ||
+      (direction === "right" && column < this.props.element.columns)
+    ) {
+      const noUpWall = [2, 3, 5, 10, 12, 13, 14];
+      const noRightWall = [1, 3, 7, 9, 11, 12, 13];
+      const noDownWall = [1, 4, 6, 10, 11, 13, 14];
+      const noLeftWall = [2, 4, 8, 9, 11, 12, 14];
+      let newTile = "";
+      let arr1 = maze[row - 1][column - 1].tile.split("");
+      let arr2 = [];
+      let currTile = "";
+      let arr3 = [];
+      if (arr1[arr1.length - 2] === "_") {
+        currTile = arr1.pop();
+      } else {
+        arr3 = arr1.splice(arr1.length - 2, 2);
+        currTile = arr3.join("");
+      }
+      switch (direction) {
+        case "up":
+          arr2 = maze[row - 2][column - 1].tile.split("");
+          if (arr2[arr2.length - 2] === "_") {
+            newTile = arr2.pop();
+          } else {
+            arr3 = arr2.splice(arr2.length - 2, 2);
+            newTile = arr3.join("");
+          }
+          break;
+        case "left":
+          arr2 = maze[row - 1][column - 2].tile.split("");
+          if (arr2[arr2.length - 2] === "_") {
+            newTile = arr2.pop();
+          } else {
+            arr3 = arr2.splice(arr2.length - 2, 2);
+            newTile = arr3.join("");
+          }
+          break;
+        case "right":
+          arr2 = maze[row - 1][column].tile.split("");
+          if (arr2[arr2.length - 2] === "_") {
+            newTile = arr2.pop();
+          } else {
+            arr3 = arr2.splice(arr2.length - 2, 2);
+            newTile = arr3.join("");
+          }
+          break;
+        case "down":
+          arr2 = maze[row][column - 1].tile.split("");
+          if (arr2[arr2.length - 2] === "_") {
+            newTile = arr2.pop();
+          } else {
+            arr3 = arr2.splice(arr2.length - 2, 2);
+            newTile = arr3.join("");
+          }
+          break;
+        default:
+          console.log("oops");
+      }
+      if (direction === "up") {
+        if (noUpWall.includes(+currTile)) {
+          if (noDownWall.includes(+newTile)) {
+            maze[row - 1][column - 1].show = "";
+            maze[row - 2][column - 1].show = <div className="dot" />;
+            this.setState({ maze: maze, row: row - 1 });
+          }
+        }
+      } else if (direction === "left") {
+        if (noLeftWall.includes(+currTile)) {
+          if (noRightWall.includes(+newTile)) {
+            maze[row - 1][column - 1].show = "";
+            maze[row - 1][column - 2].show = <div className="dot" />;
+            this.setState({ maze: maze, column: column - 1 });
+          }
+        }
+      } else if (direction === "right") {
+        if (noRightWall.includes(+currTile)) {
+          if (noLeftWall.includes(+newTile)) {
+            maze[row - 1][column - 1].show = "";
+            maze[row - 1][column].show = <div className="dot" />;
+            this.setState({ maze: maze, column: column + 1 });
+          }
+        }
+      } else if (direction === "down") {
+        if (noDownWall.includes(+currTile)) {
+          if (noUpWall.includes(+newTile)) {
+            maze[row - 1][column - 1].show = "";
+            maze[row][column - 1].show = <div className="dot" />;
+            this.setState({ maze: maze, row: row + 1 });
+          }
+        }
+      } else {
+        console.log("I broke in dot moving logic");
+      }
+    } else {
+      return;
+    }
+  }
   render() {
     return (
       <div className="play">
